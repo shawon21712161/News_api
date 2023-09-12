db9f0171b34664afde9d4d26f73f045948dff31a
shawon21712161 <sshawon19@gmail.com>
2023-09-12 23:27:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const handleCatagory =async () =>{
+   
+
+  const response = await fetch('https://openapi.programming-hero.com/api/news/categories')
+  const data = await response.json()
+
+  const tabContainer = document.getElementById('tab-conatainer');
+
+  data.data.news_category.slice(0,3).forEach((category)=>{
+    const div = document.createElement ('div');
+    div.innerHTML=`
+    <a class="tab">${category.category_name}</a>
+    `
+    tabContainer.appendChild(div)
+  }
+  )
+  console.log(data.data.news_category);
+
+}
+
+handleCatagory();
+
